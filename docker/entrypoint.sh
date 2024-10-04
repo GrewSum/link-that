92b09d3551a5c848c564af7b0499e701c31fd802
Roelof Jan <roelof.jan@hey.com>
2024-10-04 12:21:08 +0200

    DB creation test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
 
-php -r "file_exists('database/database.sqlite') || touch('database/database.sqlite')"
+FILE=database/database.sqlite
+if [ -d "$FILE" ]; then
+    # Remove the database file if it's a directory
+    rm -rf "$FILE"
+fi
+if [ ! -f "$FILE" ]; then
+    # Create the database file if it doesn't exist
+    touch "$FILE"
+fi
+
 php artisan migrate --graceful --ansi
 
 php artisan config:cache
