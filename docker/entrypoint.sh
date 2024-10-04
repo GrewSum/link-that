060b64afee829d7d77cebb4497209ca681a70183
Roelof Jan <roelof.jan@hey.com>
2024-10-04 12:15:48 +0200

    Create DB if it does not exist

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 
+php -r "file_exists('database/database.sqlite') || touch('database/database.sqlite')"
+php artisan migrate --graceful --ansi
+
 php artisan config:cache
 php artisan optimize:clear
 php artisan event:cache
 php artisan route:cache
-php artisan migrate --force
 
 if [ "${1#-}" != "$1" ]; then
 	set -- frankenphp run "$@"
